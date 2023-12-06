e7ae9aebd490228796e5077f10fc66d4f601a2c3
Abhishek <mistryabhishek2000@gmail.com>
2023-12-07 04:14:39 +0530

    Updated

--- server_boloforms/routes/categorizeRoute.js
+++ server_boloforms/routes/categorizeRoute.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const CategorizeModel = require("../models/CategorizeModel");
 const categorizeRouter = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
 
 // GET
 categorizeRouter.get('/', async (req, res) => {
   try {
     const form = await CategorizeModel.find();
-    res.status(200).send({ msg: 'All data', form })
+    res.status(200).send(form)
 
   } catch (error) {
     console.error('Error fetching forms:', error);
@@ -15,9 +19,15 @@ categorizeRouter.get('/', async (req, res) => {
 });
 
 // POST
-categorizeRouter.post('/create', async (req, res) => {
-  const formData = req.body;
+categorizeRouter.post('/create', upload.single('headerImage'), async (req, res) => {
 
+  const formData = req.body;
+  formData.items = JSON.parse(formData.items);
+
+  if (req?.file?.path) {
+    formData.headerImage = req.file.path;
+  }
+  // console.log(formData, ">>>>>>>>>>>>>>>>>>>>");
   try {
     const newForm = await CategorizeModel.create(formData);
     res.status(200).json({ msg: "New question created", payload: newForm });
